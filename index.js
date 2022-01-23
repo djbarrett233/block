fc832e9b4242be463403b38c3652a429ffe6a6bd
djbarrett233 <djbarrett233@gmail.com>
2022-01-23 19:32:48 +0000

    Inital commit

---
+++ index.js
@@ -0,0 +1,38 @@
+
+
+class NumberGuessingGame {
+
+   
+    int randomNumber = rand.nextInt(100) + 1;
+    //System.out.println("Random number is " + randomNumber);
+    
+    int tryCount = 0;
+    while(true) {
+      System.out.println("Enter your guess (1-100):");
+      
+      int playerGuess = scanner.nextInt();
+      tryCount++;
+      
+      if (playerGuess == randomNumber) {
+        System.out.println("Correct! You Win!");
+        System.out.println("It took you " + tryCount + " tries");
+        break;
+      }
+      else if(randomNumber > playerGuess) {
+        System.out.println("Nope! The number is higher. Guess again.");
+      }
+      else {
+        System.out.println("Nope! The number is lower. Guess again.");
+      }
+    }
+    
+    
+    
+    scanner.close();
+    
+  }
+}
+
+
+
+
